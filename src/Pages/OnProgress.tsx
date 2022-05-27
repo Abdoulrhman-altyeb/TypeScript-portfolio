3c7388f56094f6f02edd40cb156973dc212d3a21
Abdoulrhman-altyeb <78646930+Abdoulrhman-altyeb@users.noreply.github.com>
2022-05-27 15:28:08 +0200

    Create OnProgress.tsx

---
+++ src/Pages/OnProgress.tsx
@@ -0,0 +1,17 @@
+import {React} from 'react';
+
+
+const OnProgress:React.FC = () => {
+
+return(
+  <div> 
+    <h1>This Project is on Progress</h1>  
+    <p>I'm working on alot of projects right now with alot of new techs</p>
+  </div>
+
+)
+
+}
+
+
+export default OnProgress;
