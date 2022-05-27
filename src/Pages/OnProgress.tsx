3899670b0125be67f2a2b50e50bc85aadd9a637d
Abdoulrhman-altyeb <78646930+Abdoulrhman-altyeb@users.noreply.github.com>
2022-05-27 15:32:53 +0200

    Update OnProgress.tsx

--- src/Pages/OnProgress.tsx
+++ src/Pages/OnProgress.tsx
@@ -1,17 +1,22 @@
 import {React} from 'react';
+import styled from 'styled-components';
 
 
 const OnProgress:React.FC = () => {
 
 return(
-  <div> 
+  <onProgressProject className='container'> 
     <h1>This Project is on Progress</h1>  
     <p>I'm working on alot of projects right now with alot of new techs</p>
-  </div>
+  </onProgressProject>
 
 )
 
 }
+
+const onProgressProject = styled.div`
+  
+`;
 
 
 export default OnProgress;
